e24c88812f1754c86c3846784738899168ab71a0
Kyle Hanson <34189497+khanson66@users.noreply.github.com>
2022-02-13 01:22:39 -0500

    Improved Shell Handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod cmd;
 
-use std::time::{SystemTime};
 use std::io::Write;
+use std::process::exit;
 
 fn prompt(name:&str) -> (String, Vec<String>) {
     let mut line = String::new();
@@ -11,9 +11,10 @@ fn prompt(name:&str) -> (String, Vec<String>) {
     std::io::stdout().flush().unwrap();
     std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
 
-    let mut parts = line.trim().split_whitespace();
+    // Command and arg parsing
+    let mut parts = line.trim().split(" ");
     let command = parts.next().unwrap().to_string();
-    let args = parts;
+    let args = parts.map(|s| s.to_string()).collect();
 
     return (command, args )
 }
@@ -21,8 +22,22 @@ fn prompt(name:&str) -> (String, Vec<String>) {
 fn main() {
     loop{
         //TODO: Add formatted time to prompt
-        let input = prompt("> ");
-        println!("you put in {}",input);
+        let (command,args) = prompt("> ");
 
+        // For debugging commands
+        println!("[dbg] cmd: {} args: {}",command,args.join(","));
+
+        match command.as_str() {
+            "help" => {
+                // TODO: implement help function
+                println!("THIS IS THE HELP COMMAND")
+            },
+            "exit" =>{
+                exit(0);
+            },
+            _ =>{
+                println!("There is no such command");
+            }
+        }
     }
 }
