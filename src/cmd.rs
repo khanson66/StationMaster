25cead012f325eaa9d6179aa956c3515fd5222c0
xvxd4sh <alexongkowijoyo91@ymail.com>
2022-02-20 15:26:01 -0500

    interface work

--- src/cmd.rs
+++ src/cmd.rs
@@ -30,12 +30,13 @@ pub(crate) fn create(session_handler:&SessionHandler, mut args:Vec<String>) {
     } else {
     match args[0].to_lowercase().as_str(){
         "-s" | "-ssh" => {
-            println!("you are chosing ssh");
+            //println!("you are chosing ssh");
             let ssh_usage = "USAGE:
             create -ssh -h <hostname> -p <port>";
             let mut hostname : String = "default".to_string(); 
             let mut port: i32 = 9090;
-            if (args.len()<3){println!("{}", ssh_usage)}
+            //println!("{}",args.len());
+            if args.len()< 3 {println!("{}", ssh_usage)}
             else if args[2].is_empty() | args[4].is_empty(){
                 println!("{}", ssh_usage)
             }else{
@@ -50,7 +51,24 @@ pub(crate) fn create(session_handler:&SessionHandler, mut args:Vec<String>) {
             // TODO more input validation for insufficient inputs. 
         },
         "-nc" | "-netcat" => {
-            println!("you are chosing netcat")
+            println!("you are chosing netcat");
+            let netcat_usage = "USAGE:
+            create -netcat -n <name> -p <port>";
+            let mut name : String = "default".to_string(); 
+            let mut port: i32 = 9090;
+            if args.len()< 3 {println!("{}", netcat_usage)}
+            else if args[2].is_empty() | args[4].is_empty(){
+                println!("{}", netcat_usage)
+            }else{
+                if args[1]=="-h"{
+                    name = args[2].to_lowercase();
+                }else {println!("{}", netcat_usage)}
+                if args[3]=="-p"{
+                    port = args[4].parse::<i32>().unwrap();
+                }else{println!("{}", netcat_usage)}
+            }
+            println!("name: {} & port: {}", name, port);
+           
         },
         _ => {println!("{}",create_help)}
         }
