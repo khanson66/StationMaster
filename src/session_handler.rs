6da251b41b1085ff8d59cca5a3f66a9c7553b0ca
Kyle <kh99944p@pace.edu>
2022-02-27 13:22:26 -0500

    Sending Commands and Setting active session

--- src/session_handler.rs
+++ src/session_handler.rs
@@ -2,13 +2,14 @@ use crate::sessions::raw_socket::RawSocket;
 use crate::SESSION;
 use std::fmt;
 use std::fs::OpenOptions;
+use std::ops::Deref;
 use std::thread::current;
 
 pub(crate) struct SessionHandler {
     sessions: Vec<SessionTypes>,
     //NOTE: needed to simplify the duplicate check and positioning.
     names: Vec<String>, //TODO: Find a way to remove this
-    active_session: Option<dyn SESSION>,
+    active_session: Option<SessionTypes>
 }
 
 #[derive(Debug)]
@@ -31,11 +32,13 @@ impl SessionHandler {
 
     //TODO: Create method to list all sessions
     pub fn list_sessions(&self) {
-        if self.sessions.len() <= 0 && self.active_session == None {
+        if self.sessions.len() <= 0 && self.active_session.is_none() {
             println!("There are no active sessions");
         } else {
-            if self.active_session != None{
-                println!("{}", self.active_session.as_ref().unwrap().get_name())
+            if self.active_session.is_some(){
+                match self.active_session.as_ref().unwrap(){
+                    SessionTypes::RawSocket(s) => println!("{:?}", s.to_string()),
+                };
             }
             for i in &self.sessions {
                 match i {
@@ -66,21 +69,46 @@ impl SessionHandler {
         };
     }
 
-    pub fn set_active_session(&self, name: String) {
-        if self.sessions.len() <= 0 && self.active_session == None {
+    pub fn set_active_session(&mut self, name: String) {
+        if self.sessions.len() <= 0 && self.active_session.is_none() {
             println!("There are no active sessions");
         } else {
-            for i in &self.sessions {
-                match i {
+            for i in 0..self.sessions.len() {
+                let si = self.sessions.get(i).unwrap();
+                match si {
                     SessionTypes::RawSocket(s) => {
                         if name == s.get_name(){
 
+                            let temp =
+                                Option::from(self.sessions.remove(i));
+
+                            match &self.active_session{
+                                Some(T) => {
+                                    let old_active_session =
+                                        std::mem::replace(&mut self.active_session, temp);
+
+                                    self.sessions.push(old_active_session.unwrap());
+                                }
+                                None => {
+                                    //Place holder incase logic is needed
+                                    todo!()
+                                }
+
+                            }
+                            return;
                         }
-                    },
+                    }
                 }
             }
+            println!("[!] Cant set active session because one does not exist");
         }
     }
 
-
+    pub(crate) fn send_command(&self, cmd: String){
+        match &self.active_session.as_ref().unwrap() {
+            SessionTypes::RawSocket(s) => {
+                s.send_command(cmd);
+            }
+        }
+    }
 }
